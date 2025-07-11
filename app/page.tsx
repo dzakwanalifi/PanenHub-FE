7f007dc11940f2ce822e394ba292e3513e50533e
Dzakwan Alifi <65795917+dzakwanalifi@users.noreply.github.com>
2025-07-11 21:41:14 +0700

    Fix hydration error on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 'use client';
+import ClientOnly from '@/components/ClientOnly';
 import HeroSection from '@/components/sections/home/HeroSection';
 import CategoriesSection from '@/components/sections/home/CategoriesSection';
 import FeaturedProductsSection from '@/components/sections/home/FeaturedProductsSection';
 
 export default function Home() {
   return (
-    <div className="space-y-8">
-      <HeroSection />
-      <CategoriesSection />
-      <FeaturedProductsSection />
-    </div>
+    <ClientOnly>
+      <div className="space-y-8">
+        <HeroSection />
+        <CategoriesSection />
+        <FeaturedProductsSection />
+      </div>
+    </ClientOnly>
   );
 }
